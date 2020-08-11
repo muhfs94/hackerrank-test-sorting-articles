2f660234372f26d6def313c75cf406cb05b08253
Muhammad <muhfs94@gmail.com>
2020-08-11 10:24:42 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+const sortByMostUpvotes = (array) => {
+  return array.sort((a, b) => b.upvotes - a.upvotes);
+};
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
+const sortByLeastUpvotes = (array) => {
+  return array.sort((a, b) => a.upvotes - b.upvotes);
+};
 
-}
+const sortByMostRecent = (array) => {
+  return array.sort((a, b) => new Date(b.date) - new Date(a.date));
+};
+
+const App = (props) => {
+  const [articles, setArticles] = useState(sortByMostUpvotes(props.articles));
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     articles: sortByMostUpvotes(this.props.articles),
+  //   };
+  // }
+
+  const handleMostUpvotes = () => {
+    const newArticles = [...articles];
+    setArticles(sortByMostUpvotes(newArticles));
+    // this.setState({ articles: sortByMostUpvotes(this.state.articles) });
+  };
+
+  const handleMostRecent = () => {
+    const newArticles = [...articles];
+    setArticles(sortByMostRecent(newArticles));
+    // this.setState({ articles: sortByMostRecent(this.state.articles) });
+  };
+
+  // render() {
+  return (
+    <div className='App'>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className='layout-row align-items-center justify-content-center my-20 navigation'>
+        <label className='form-hint mb-0 text-uppercase font-weight-light'>
+          Sort By
+        </label>
+        <button
+          data-testid='most-upvoted-link'
+          className='small'
+          onClick={handleMostUpvotes}
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid='most-recent-link'
+          className='small'
+          onClick={handleMostRecent}
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={articles} />
+    </div>
+  );
+  // }
+};
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+function Articles(props) {
+  return (
+    <div className='card w-50 mx-auto'>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.articles.map(({ title, upvotes, date }) => (
+            <tr data-testid='article' key={title}>
+              <td data-testid='article-title'>{title}</td>
+              <td data-testid='article-upvotes'>{upvotes}</td>
+              <td data-testid='article-date'>{date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
